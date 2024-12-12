89c3c7240c56145b87852da89b8c29feb76b67a3
birukabza <birukabza@gmail.com>
2024-12-12 22:16:06 +0300

    added a line break between the sidebar and the rest of the elements. also added the line break on the userProfile page to separate it from the posts

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -6,6 +6,7 @@ const Layout = () => {
   return (
     <div className="flex bg-primaryLight2 w-full h-screen">
       <SideBar/>
+      <div className="border-r border-gray-700 h-full w-[calc(6rem+1px)]"></div>
       <div className="flex-grow">
         <Outlet />
       </div>
--- client/src/pages/user-profile/UserProfile.jsx
+++ client/src/pages/user-profile/UserProfile.jsx
@@ -5,11 +5,14 @@ import { useParams } from "react-router-dom";
 
 const UserProfile = () => {
 
-    const {username} = useParams()
+    const { username } = useParams()
 
     return (
-        <div className="ml-40 p-6 flex flex-row gap-12 max-w-5xl w-full">
-            <UserDetails username={username}/>
+        <div className="flex flex-col ml-40 mr-80 pt-12">
+            <div className="flex flex-row gap-12 max-w-5xl w-full">
+                <UserDetails username={username} />
+            </div>
+            <div className="border-b border-gray-700 w-full] mt-10"></div>
         </div>
     );
 };
