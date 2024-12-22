5bd592e9ed1dc84e1e089a3af98d309861934978
birukabza <birukabza@gmail.com>
2024-12-22 23:29:43 +0300

    Ui change of the sidebar

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = () => {
   return (
     <div className="flex flex-row bg-primaryLight2 w-screen min-h-screen gap-1">
       <SideBar />
-      <div className="bg-gray-700 w-[calc(6rem+1px)] min-h-screen fixed"></div> 
+      <div className="bg-gray-700 w-[calc(9rem+1px)] min-h-screen fixed"></div> 
       <div className="flex-grow pl-[calc(6rem+1px)]">
         <Outlet />
         </div> 
--- client/src/components/side-bar/SideBar.jsx
+++ client/src/components/side-bar/SideBar.jsx
@@ -4,6 +4,8 @@ import logo from "../../assets/Meet-Me-Logo.png";
 
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { FaSearch } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+import { IoIosPeople } from "react-icons/io";
 
 import { Link } from "react-router-dom";
 
@@ -17,14 +19,23 @@ const SideBar = () => {
   const redirectTo = isAuthenticated ?  `profile/${currentUsername}`  : "/signin"
 
   return (
-    <div className="flex flex-col fixed top-0 left-0 h-screen w-24 text-white bg-primary shadow-xl p-4 z-10">
+    
+    <div className="flex flex-col fixed top-0 left-0 h-screen w-36 text-white bg-primary shadow-xl p-4 z-10 items-center gap-6  pt-8">
       <Link to="/">
-        <SideBarIcons icon={logo} logo />
+      <img src={logo} alt="Company Logo" className="size-16 shadow-sm shadow-gray-700 rounded-lg mb-4"/>
+      </Link>
+      <Link to="/">
+        <SideBarIcons icon={<FaHome size="25" />} text="Home" />
+      </Link>
+      <Link>
+      <SideBarIcons icon={<FaSearch size="25" />} text="Search" />
+      </Link>
+      <Link>
+      <SideBarIcons icon={<IoIosPeople size="25" />} text="Friends" />
       </Link>
       <Link to={redirectTo}>
-        <SideBarIcons icon={<IoPersonCircleSharp size="50" />} text="Profile" />
+        <SideBarIcons icon={<IoPersonCircleSharp size="30" />} text="Profile" />
       </Link>
-      <SideBarIcons icon={<FaSearch size="40" />} text="Search" />
     </div>
   );
 };
