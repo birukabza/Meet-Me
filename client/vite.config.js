7aa7e06db3c63b4712428b5c4a322cc65fec5773
birukabza <birukabza@gmail.com>
2025-01-03 20:39:31 +0300

    adding event listeners to the proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,10 +14,22 @@ export default defineConfig(({ mode }) => {
   return {
     server: {
       proxy: {
-        "/api": {
-          target: target,
+        '/api': {
+          target: target, // Replace with your backend server URL
           changeOrigin: true,
           secure: false,
+          ws: true,
+          configure: (proxy, _options) => {
+            proxy.on('error', (err, _req, _res) => {
+              console.error('Proxy error:', err);
+            });
+            proxy.on('proxyReq', (proxyReq, req, _res) => {
+              console.log('Sending Request to the Target:', req.method, req.url);
+            });
+            proxy.on('proxyRes', (proxyRes, req, _res) => {
+              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+            });
+          },
         },
       },
     },
