ffc5f545344e30059c712dfa8088b333f9555797
birukabza <birukabza@gmail.com>
2025-01-03 12:47:34 +0300

    setting up the frontend for deployment

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,2 +1,6 @@
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL
-export const CLOUDINARY_BASE_URL = import.meta.env.VITE_CLOUDINARY_BASE_URL
+export const SERVER_URL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_PRODUCTION_SERVER_URL
+    : import.meta.env.VITE_SERVER_URL;
+
+export const CLOUDINARY_BASE_URL = import.meta.env.VITE_CLOUDINARY_BASE_URL;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',  
-        changeOrigin: true,  
-        secure: true,      
+
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef, no-unused-vars
+  const env = loadEnv(mode, process.cwd());
+
+  const target =
+    mode === "production"
+      ? "https://meet-me-backend.onrender.com/"
+      : "http://127.0.0.1:8000";
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: target,
+          changeOrigin: true,
+          secure: true,
+        },
       },
     },
-  },
-})
+  };
+});
