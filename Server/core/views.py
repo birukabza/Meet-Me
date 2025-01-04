beb266c085bf4f974b2edcf0c962171b9c25f6d6
birukabza <birukabza@gmail.com>
2025-01-04 12:33:27 +0300

    removing  domain attribute in set_cookie in views.py

--- Server/core/views.py
+++ Server/core/views.py
@@ -468,7 +468,6 @@ class CustomTokenObtainPairView(TokenObtainPairView):
                 samesite="None",
                 secure=True,
                 path="/",
-                domain="https://meet-me-lx8v.onrender.com"
             )
             res.set_cookie(
                 key="refresh_token",
@@ -477,7 +476,6 @@ class CustomTokenObtainPairView(TokenObtainPairView):
                 samesite="None",
                 secure=True,
                 path="/",
-                domain="https://meet-me-lx8v.onrender.com"
             )
             return res
 
@@ -537,7 +535,6 @@ class CustomTokenRefreshView(TokenRefreshView):
                 secure=True,
                 samesite="None",
                 path="/",
-                domain="https://meet-me-lx8v.onrender.com"
             )
 
             return res
