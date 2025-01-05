6b6820a72e21d61252764a7d80340fdcfdc98538
birukabza <birukabza@gmail.com>
2025-01-05 21:42:20 +0300

    added commands to populate the database

--- Server/build.sh
+++ Server/build.sh
@@ -6,6 +6,11 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
+# only one time for populating the data base
+python manage.py create_users
+
+python manage.py create_posts
+
 # needed for the first time to create superuser
 # if [[ $CREATE_SUPERUSER ]];
 # then
