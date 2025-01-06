71e5c330d6de07492712e17ca8eba49852655a5b
birukabza <birukabza@gmail.com>
2025-01-06 22:43:39 +0300

    adjusted homeview of the server

--- Server/Server/home_view.py
+++ Server/Server/home_view.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home_view(request):
-    return HttpResponse("<h1> <center> This is Meet-me-backend <center/><h1/>")
+    return HttpResponse("<h1> <center> This is Meet-me-backend <center/><h1/><br/> <h3> You can access the frontend here <a href='https://meet-me-lx8v.onrender.com'>Frontend<a/><h3/>")
